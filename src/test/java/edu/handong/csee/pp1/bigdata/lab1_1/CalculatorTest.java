2fb276b2ec78ac28a129c52971c030e89756a68a
dpcks <dpcks@LEEYECHAN>
2018-10-03 20:16:34 +0900

    #5 assertEquals

--- src/test/java/edu/handong/csee/pp1/bigdata/lab1_1/CalculatorTest.java
+++ src/test/java/edu/handong/csee/pp1/bigdata/lab1_1/CalculatorTest.java
@@ -11,7 +11,7 @@ public class CalculatorTest {
 		String[] args = {"2", "3"};
 		Calculator.main(args);
 		// assertEquals compares the result of an method call with the expected result.
-		assertEquals(Calculator.getCount(),4);
+		assertEquals(Calculator.getCount(),Calculator.countForAnyCompution);
 	}
 	
 	@Test
@@ -28,7 +28,8 @@ public class CalculatorTest {
 	
 	@Test
 	public void testMod() {
-		// TODO Complete this test case
+		Calculator myCalc = new Calculator();
+		assertEquals(myCalc.mod(2, 3),2);
 	}
 
 	@Test
@@ -39,7 +40,7 @@ public class CalculatorTest {
 	@Test
 	public void testDivide() {
 		Calculator myCalc = new Calculator();
-		assertEquals(myCalc.divide(1, 1),1, 0.00000001);
-		assertEquals(myCalc.divide(2, 2),Double.NaN, 0.00000001);
+		assertEquals(myCalc.divide(1, 1),1);
+	//	assertEquals(myCalc.divide(2, 2),Double.NaN, 0.00000001);
 	}
 }
